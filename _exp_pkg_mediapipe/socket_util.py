83516b3b3196303cf19b2068faa8ac6808ecd3c1
EarlyAnt <54763755@qq.com>
2022-04-23 09:08:11 +0800

    1.socket_util.py，完善了测试用的几个方法，但还是只能本地起服务器让客户端连，连远端的服务器连不上。

--- _exp_pkg_mediapipe/socket_util.py
+++ _exp_pkg_mediapipe/socket_util.py
@@ -1,35 +1,38 @@
+from http import server
 import socket
 import threading
 from time import sleep
 
-ip = "192.168.0.106"
-port = 8000
+default_ip = "192.168.0.106"
+default_port = 8080
 
 class SocketClient:
     #定义一个ip协议版本AF_INET，为IPv4；同时也定义一个传输协议（TCP）SOCK_STREAM
     _client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     _connected = False
     
-    def connect(self, server_ip_port=(ip,port)):
+    def connect(self, server_ip_port=(default_ip,default_port)):
         #进行连接服务器
         try:
+            print("->client try to connect: {}".format(server_ip_port))
             self._client.connect(server_ip_port)
             self._connected = True
+            print("->client connected to server")
             self.send("i am client")
             
             while True:
                 received_data=self._client.recv(1024)#接受服务端的信息，最大数据为1k
                 print("->client receive data: %s" % received_data.decode('utf-8'))
                 message=input("client wanna say:")
-                self.send(message.encode('utf-8'))
+                self.send(message)
         except Exception as ex:
             print("->connect to server error: %s" % ex)
 
     def send(self, message):
         try:
             if self._client:
-                message = str(message).encode("utf-8")
-                self._client.send(message)#将发送的数据进行编码
+                bytes = str(message).encode("utf-8")
+                self._client.send(bytes)#将发送的数据进行编码
                 print("->client send data: %s" % message)
         except Exception as ex:
             print("->send message error: %s" % ex)
@@ -45,10 +48,11 @@ class SocketClient:
 class SocketServer:
     _server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     
-    def run_server(self, ip_port=(ip,port)):
+    def run_server(self, server_ip_port=(default_ip,default_port)):
         try:
-            self._server.bind(ip_port)
+            self._server.bind(server_ip_port)
             self._server.listen(3)
+            print("->server[{}] is running...".format(server_ip_port))
             client, _=self._server.accept()
             print(client)
         
@@ -68,21 +72,26 @@ class SocketServer:
             print("->stop server error: %s" % ex)
 
 
-def run_server():
+def run_server(ip_port=(default_ip,default_port)):
     server = SocketServer()
-    # server.run_server(ip_port=("192.168.0.6", 8000))
-    server.run_server()
+    server.run_server(server_ip_port=ip_port)
     
-def run_client():
+def run_client(ip_port=(default_ip,default_port)):
     client = SocketClient();
-    # client.connect(server_ip_port=("192.168.0.6", 8000))
-    client.connect()
-
-if __name__ == "__main__":
-    t1 = threading.Thread(target=run_server)     # target是要执行的函数名（不是函数），args是函数对应的参数，以元组的形式存在
-    t2 = threading.Thread(target=run_client)
+    client.connect(server_ip_port=ip_port)
+    
+def connect_local(ip_port=(default_ip,default_port)):
+    t1 = threading.Thread(target=run_server, args=(ip_port,))     # target是要执行的函数名（不是函数），args是函数对应的参数，以元组的形式存在
+    t2 = threading.Thread(target=run_client, args=(ip_port,))
     t1.start()
     sleep(1)
     t2.start()
+    
+def connect_remote(ip_port=(default_ip,default_port)):
+    run_client(ip_port=ip_port);
+
+if __name__ == "__main__":
+    connect_local()
+    # connect_remote(ip_port=("192.168.0.5", 8080))
 
 
