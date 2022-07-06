b03dc39f51ea702143791884c6549bc6c1fa7141
EarlyAnt <54763755@qq.com>
2022-07-06 20:33:40 +0800

    1.dd_setting.py脚本，为避免冲突，修改变量名。 2.prompt_engine.py脚本，将逻辑封装成类。

--- _exp_simple_test/dd_setting.py
+++ _exp_simple_test/dd_setting.py
@@ -6,48 +6,44 @@ SettingParam = namedtuple(
 
 StyleConfig = namedtuple("StyleConfig", "artists, prefix")
 
-artists = {'0101': 'Chinese ink painting',
-           '0201': 'Aka mike winkelman',
-           '0202': 'high-definition',
-           '0203': 'Unreal Engine',
-           '0301': 'Jacek Yerka',
-           '0302': 'Rene Magritte',
-           '0303': 'Igor Morski',
-           '0401': 'Simon Stalenhag',
-           '0402': 'Ross Tran',
-           '0403': 'Liam WongJohn Harris',
-           '0404': 'Thomas Kinkade',
-           '0501': 'Pascal Campion'}
+artist_list = {'0101': 'Chinese ink painting',
+               '0201': 'Aka mike winkelman',
+               '0202': 'high-definition',
+               '0203': 'Unreal Engine',
+               '0301': 'Jacek Yerka',
+               '0302': 'Rene Magritte',
+               '0303': 'Igor Morski',
+               '0401': 'Simon Stalenhag',
+               '0402': 'Ross Tran',
+               '0403': 'Liam WongJohn Harris',
+               '0404': 'Thomas Kinkade',
+               '0501': 'Pascal Campion'}
 
-styles = {0: None,
-          1: StyleConfig(artists=['0101'], prefix='A beautiful Chinese ink'),
-          2: StyleConfig(artists=['0201', '0202', '0203'], prefix='A beautiful VR 3D painting by '),
-          3: StyleConfig(artists=['0301', '0302', '0303'], prefix='A picture by '),
-          4: StyleConfig(artists=['0401', '0402', '0403', '0404'], prefix='A image by '),
-          5: StyleConfig(artists=['0401', '0402', '0403', '0404'], prefix='A modern picture by ')}
+style_list = {0: None,
+              1: StyleConfig(artists=['0101'], prefix='A beautiful Chinese ink'),
+              2: StyleConfig(artists=['0201', '0202', '0203'], prefix='A beautiful VR 3D painting by '),
+              3: StyleConfig(artists=['0301', '0302', '0303'], prefix='A picture by '),
+              4: StyleConfig(artists=['0401', '0402', '0403', '0404'], prefix='A image by '),
+              5: StyleConfig(artists=['0401', '0402', '0403', '0404'], prefix='A modern picture by ')}
+
+rule_list = {}
+rule_list["0101"] = SettingParam(
+    display_rate=1, n_batches=1, batch_size=1, steps=1, skip_steps=1, cut_overview=1, cut_innercut=1, cut_ic_pow=1, cut_icgray_p=1)
+rule_list["0201"] = SettingParam(
+    display_rate=2, n_batches=2, batch_size=2, steps=2, skip_steps=2, cut_overview=2, cut_innercut=2, cut_ic_pow=2, cut_icgray_p=2)
+rule_list["0202"] = SettingParam(
+    display_rate=3, n_batches=3, batch_size=3, steps=3, skip_steps=3, cut_overview=3, cut_innercut=3, cut_ic_pow=3, cut_icgray_p=3)
+rule_list["0203"] = SettingParam(
+    display_rate=4, n_batches=4, batch_size=4, steps=4, skip_steps=4, cut_overview=4, cut_innercut=4, cut_ic_pow=4, cut_icgray_p=4)
 
 
 def get_suffix():
     return 'Trending on artstation.'
 
 
-def get_rules():
-    rules = {}
-    rules["0101"] = SettingParam(
-        display_rate=1, n_batches=1, batch_size=1, steps=1, skip_steps=1, cut_overview=1, cut_innercut=1, cut_ic_pow=1, cut_icgray_p=1)
-    rules["0201"] = SettingParam(
-        display_rate=2, n_batches=2, batch_size=2, steps=2, skip_steps=2, cut_overview=2, cut_innercut=2, cut_ic_pow=2, cut_icgray_p=2)
-    rules["0202"] = SettingParam(
-        display_rate=3, n_batches=3, batch_size=3, steps=3, skip_steps=3, cut_overview=3, cut_innercut=3, cut_ic_pow=3, cut_icgray_p=3)
-    rules["0203"] = SettingParam(
-        display_rate=4, n_batches=4, batch_size=4, steps=4, skip_steps=4, cut_overview=4, cut_innercut=4, cut_ic_pow=4, cut_icgray_p=4)
-
-    return rules
-
-
 def get_setting(style_code):
     print("get_setting->style_code: {}".format(style_code))
-    style = styles[style_code]
+    style = style_list[style_code]
     if style is not None and len(style.artists) > 0:
         print("get_setting->style: {}".format(style))
         artists_list = style.artists
@@ -57,8 +53,7 @@ def get_setting(style_code):
         artist = artists_list[index]
         print("get_setting->artist: {}".format(artist))
 
-        rules = get_rules()
-        setting_param = rules[artist]
+        setting_param = rule_list[artist]
         print("get_setting->setting_param: {}".format(setting_param))
         return setting_param
     else:
@@ -71,7 +66,7 @@ def make_image(text_prompt, style):
 
     print("make_image->text_prompt: {}".format(text_prompt))
     print("make_image->style: {}".format(style))
-    style = styles[style]
+    style = style_list[style]
     artist = ''
     if style is not None and len(style.artists) > 0:
         artists_list = style.artists
@@ -80,7 +75,7 @@ def make_image(text_prompt, style):
         print("make_image->index: {}".format(index))
         artist_code = artists_list[index]
         print("make_image->artist_code: {}".format(artist_code))
-        artist = artists[artist_code]
+        artist = artist_list[artist_code]
         print("make_image->artist: {}".format(artist))
 
     print("make_image->full prompt: {}{}, {}. {}".format(style.prefix,
---
+++ _exp_simple_test/prompt_engine.py
@@ -0,0 +1,90 @@
+import random
+from collections import namedtuple
+
+SettingParam = namedtuple(
+    "SettingParam", "display_rate, n_batches, batch_size, steps, skip_steps, cut_overview, cut_innercut, cut_ic_pow, cut_icgray_p")
+
+StyleConfig = namedtuple("StyleConfig", "artists, prefix")
+
+
+class PromptEngine:
+    _artist_list = {'0101': 'Chinese ink painting',
+                    '0201': 'Aka mike winkelman',
+                    '0202': 'high-definition',
+                    '0203': 'Unreal Engine',
+                    '0301': 'Jacek Yerka',
+                    '0302': 'Rene Magritte',
+                    '0303': 'Igor Morski',
+                    '0401': 'Simon Stalenhag',
+                    '0402': 'Ross Tran',
+                    '0403': 'Liam WongJohn Harris',
+                    '0404': 'Thomas Kinkade',
+                    '0501': 'Pascal Campion'}
+
+    _style_list = {0: None,
+                   1: StyleConfig(artists=['0101'], prefix='A beautiful Chinese ink'),
+                   2: StyleConfig(artists=['0201', '0202', '0203'], prefix='A beautiful VR 3D painting by '),
+                   3: StyleConfig(artists=['0301', '0302', '0303'], prefix='A picture by '),
+                   4: StyleConfig(artists=['0401', '0402', '0403', '0404'], prefix='A image by '),
+                   5: StyleConfig(artists=['0401', '0402', '0403', '0404'], prefix='A modern picture by ')}
+
+    _rule_list = {}
+    _rule_list["0101"] = SettingParam(
+        display_rate=1, n_batches=1, batch_size=1, steps=1, skip_steps=1, cut_overview=1, cut_innercut=1, cut_ic_pow=1, cut_icgray_p=1)
+    _rule_list["0201"] = SettingParam(
+        display_rate=2, n_batches=2, batch_size=2, steps=2, skip_steps=2, cut_overview=2, cut_innercut=2, cut_ic_pow=2, cut_icgray_p=2)
+    _rule_list["0202"] = SettingParam(
+        display_rate=3, n_batches=3, batch_size=3, steps=3, skip_steps=3, cut_overview=3, cut_innercut=3, cut_ic_pow=3, cut_icgray_p=3)
+    _rule_list["0203"] = SettingParam(
+        display_rate=4, n_batches=4, batch_size=4, steps=4, skip_steps=4, cut_overview=4, cut_innercut=4, cut_ic_pow=4, cut_icgray_p=4)
+
+    def _get_suffix(self):
+        return 'Trending on artstation.'
+
+    def get_rule(self, style):
+        print("get_rule->style: {}".format(style))
+        style = self._style_list[style]
+        if style is not None and len(style.artists) > 0:
+            artists_list = style.artists
+            print("get_rule->artists_list: {}".format(artists_list))
+            index = random.randint(0, len(artists_list) - 1)
+            print("get_rule->index: {}".format(index))
+            artist_code = artists_list[index]
+            print("get_rule->artist_code: {}".format(artist_code))
+            rule = self._rule_list[artist_code]
+            print("get_rule->rule: {}".format(rule))
+            return rule
+        else:
+            print("get_rule->none rule")
+            return None
+
+    def make_sentense(self, text_prompt, style):
+        print("make_sentense->text_prompt: {}".format(text_prompt))
+        print("make_sentense->style: {}".format(style))
+        style = self._style_list[style]
+        artist = ''
+        if style is not None and len(style.artists) > 0:
+            artists_list = style.artists
+            print("make_sentense->artists_list: {}".format(artists_list))
+            index = random.randint(0, len(artists_list) - 1)
+            print("make_sentense->index: {}".format(index))
+            artist_code = artists_list[index]
+            print("make_sentense->artist_code: {}".format(artist_code))
+            artist = self._artist_list[artist_code]
+            print("make_sentense->artist: {}".format(artist))
+
+        sentence = "{}{}, {}. {}".format(
+            style.prefix, artist, text_prompt, self._get_suffix())
+        print("make_sentense->full prompt: {}".format(sentence))
+        return sentence
+
+
+if __name__ == "__main__":
+    prompt_engine = PromptEngine()
+
+    sentence = prompt_engine.make_sentense(
+        text_prompt="a group of small animals are feeding, playing and resting on the grass", style=2)
+    print("main->sentence: {}".format(sentence))
+
+    rule = prompt_engine.get_rule(style=2)
+    print("main->rule.step: {}".format(rule.steps))
