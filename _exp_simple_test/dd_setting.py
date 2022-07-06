0ccbc234e224ea5902ca8ab10320e6955f6382e1
EarlyAnt <54763755@qq.com>
2022-07-06 19:36:40 +0800

    1.dd_setting.py脚本，实现了按照固定句式，拼接句子的前缀和后缀的功能。

--- _exp_simple_test/dd_setting.py
+++ _exp_simple_test/dd_setting.py
@@ -1,5 +1,13 @@
 import random
 from collections import namedtuple
+from matplotlib.pyplot import text
+
+from sqlalchemy import null
+
+SettingParam = namedtuple(
+    "SettingParam", "display_rate, n_batches, batch_size, steps, skip_steps, cut_overview, cut_innercut, cut_ic_pow, cut_icgray_p")
+
+StyleConfig = namedtuple("StyleConfig", "artists, prefix")
 
 artists = {'0101': 'Chinese ink painting',
            '0201': 'Aka mike winkelman',
@@ -14,14 +22,16 @@ artists = {'0101': 'Chinese ink painting',
            '0404': 'Thomas Kinkade',
            '0501': 'Pascal Campion'}
 
-styles = {0: [],
-          1: ['0101'],
-          2: ['0201', '0202', '0203'],
-          3: ['0301', '0302', '0303'],
-          4: ['0401', '0402', '0403', '0404']}
+styles = {0: null,
+          1: StyleConfig(artists=['0101'], prefix='A beautiful Chinese ink'),
+          2: StyleConfig(artists=['0201', '0202', '0203'], prefix='A beautiful VR 3D painting by '),
+          3: StyleConfig(artists=['0301', '0302', '0303'], prefix='A picture by '),
+          4: StyleConfig(artists=['0401', '0402', '0403', '0404'], prefix='A image by '),
+          5: StyleConfig(artists=['0401', '0402', '0403', '0404'], prefix='A modern picture by ')}
 
-SettingParam = namedtuple(
-    "SettingParam", "display_rate, n_batches, batch_size, steps, skip_steps, cut_overview, cut_innercut, cut_ic_pow, cut_icgray_p")
+
+def get_suffix():
+    return 'Trending on artstation.'
 
 
 def get_rules():
@@ -38,28 +48,52 @@ def get_rules():
     return rules
 
 
-def get_setting(style):
-    style_int = int(style)
-    print("get_setting->style: {}".format(style_int))
-    artists_list = styles[style_int]
-    print("get_setting->artists_list: {}".format(artists_list))
-    index = random.randint(0, len(artists_list) - 1)
-    print("get_setting->index: {}".format(index))
-    artist = artists_list[index]
-    print("get_setting->artist: {}".format(artist))
+def get_setting(style_code):
+    print("get_setting->style_code: {}".format(style_code))
+    style = styles[style_code]
+    if style is not null and len(style.artists) > 0:
+        print("get_setting->style: {}".format(style))
+        artists_list = style.artists
+        print("get_setting->artists_list: {}".format(artists_list))
+        index = random.randint(0, len(artists_list) - 1)
+        print("get_setting->index: {}".format(index))
+        artist = artists_list[index]
+        print("get_setting->artist: {}".format(artist))
 
-    rules = get_rules()
-    setting_param = rules[artist]
-    print("get_setting->setting_param: {}".format(setting_param))
-    return setting_param
+        rules = get_rules()
+        setting_param = rules[artist]
+        print("get_setting->setting_param: {}".format(setting_param))
+        return setting_param
+    else:
+        print("get_setting->none artist")
+        return null
 
 
-def make_image(dd_setting):
+def make_image(text_prompt, style):
     print("make_image->start ================================================")
-    print("make_image->dd_setting: {}".format(dd_setting))
+
+    print("make_image->text_prompt: {}".format(text_prompt))
+    print("make_image->style: {}".format(style))
+    style = styles[style]
+    artist = ''
+    if style is not null and len(style.artists) > 0:
+        artists_list = style.artists
+        print("make_image->artists_list: {}".format(artists_list))
+        index = random.randint(0, len(artists_list) - 1)
+        print("make_image->index: {}".format(index))
+        artist_code = artists_list[index]
+        print("make_image->artist_code: {}".format(artist_code))
+        artist = artists[artist_code]
+        print("make_image->artist: {}".format(artist))
+
+    print("make_image->full prompt: {}{}, {}. {}".format(style.prefix,
+          artist, text_prompt, get_suffix()))
+
     print("make_image->complete ---------------------------------------------")
 
 
 if __name__ == "__main__":
-    setting_param = get_setting(style=2)
-    make_image(setting_param)
+    # get_setting(style_code=2)
+
+    make_image(
+        text_prompt="a group of small animals are feeding, playing and resting on the grass", style=2)
